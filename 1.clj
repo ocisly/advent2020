0fa9a3106ed86480c291660146da0dfbd27c835f
Adam Thalhammer <1496836+ocisly@users.noreply.github.com>
2020-12-02 10:11:46 +1100

    Solve puzzle 1, part 2, inefficiently

--- 1.clj
+++ 1.clj
@@ -6,7 +6,7 @@
   (if-let [diff (diffs number)]
     (reduced [diff number])
     diffs))
-  
+
 
 (def diff-2020 (partial - 2020))
 
@@ -15,4 +15,19 @@
         [x y] (reduce sum-to diffs numbers)]
     (* x y)))
 
+(defn debug [x]
+  (prn x)
+  x)
+
+(defn puzzle2 [numbers]
+  (let [n (count numbers)]
+    (first
+      (for [i (range n) :let [x (nth numbers i)]
+            j (range (inc i) n) :let [y (nth numbers j)]
+            k (range (inc j) n) :let [z (nth numbers k)]
+            :when (= 2020 (+ x y z))]
+        (* x y z)))))
+
 (comment (time (puzzle1 input)))
+(comment (time (puzzle2 [1721 979 366 299 675 1456])))
+(comment (time (puzzle2 input)))
