fb4932a99059b40b722271211cb887d1efc942f2
Adam Thalhammer <1496836+ocisly@users.noreply.github.com>
2020-12-02 16:31:46 +1100

    solve puzzle 2 part 1

---
+++ 2.clj
@@ -0,0 +1,22 @@
+(defn debug [& x] (apply prn x) x)
+
+(def input (->> (slurp "input2.txt")))
+
+(defn parse [[low high character password]]
+  {:low (Integer/parseInt low)
+   :high (Integer/parseInt high)
+   :character (first character)
+   :password password})
+
+(defn valid-password? [{:keys [low high character password]}]
+  (<= low (get (frequencies password) character 0) high))
+
+(defn puzzle1 [passwords]
+  (->> passwords
+    (re-seq #"(\d+)-(\d+) (\w): (\w+)")
+    (map (comp parse rest))
+    (filter valid-password?)
+    (count)))
+
+(comment (time (puzzle1 "1-3 a: abcde\n1-3 b: cdefg\n2-9 c: ccccccccc\n")))
+(comment (time (puzzle1 input)))
