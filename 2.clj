b7f7364276832b99075ae95cb5a9c8de231debe4
Adam Thalhammer <1496836+ocisly@users.noreply.github.com>
2020-12-02 16:42:09 +1100

    solve puzzle 2 part 2

--- 2.clj
+++ 2.clj
@@ -11,6 +11,10 @@
 (defn valid-password? [{:keys [low high character password]}]
   (<= low (get (frequencies password) character 0) high))
 
+(defn valid-positional-password? [{:keys [low high character password]}]
+  (let [characters (map (comp (partial nth password) dec) [low high])]
+    (= 1 (count (filter #{character} characters)))))
+
 (defn puzzle1 [passwords]
   (->> passwords
     (re-seq #"(\d+)-(\d+) (\w): (\w+)")
@@ -18,5 +22,15 @@
     (filter valid-password?)
     (count)))
 
+(defn puzzle2 [passwords]
+  (->> passwords
+    (re-seq #"(\d+)-(\d+) (\w): (\w+)")
+    (map (comp parse rest))
+    (filter valid-positional-password?)
+    (count)))
+
 (comment (time (puzzle1 "1-3 a: abcde\n1-3 b: cdefg\n2-9 c: ccccccccc\n")))
 (comment (time (puzzle1 input)))
+
+(comment (time (puzzle2 "1-3 a: abcde\n1-3 b: cdefg\n2-9 c: ccccccccc\n")))
+(comment (time (puzzle2 input)))
