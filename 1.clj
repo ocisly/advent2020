8a213edaa4bd7b92c39a94fdc3473b0686d6027d
Adam Thalhammer <1496836+ocisly@users.noreply.github.com>
2020-12-02 11:29:14 +1100

    Refactor, efficient implementation of part II

--- 1.clj
+++ 1.clj
@@ -1,32 +1,26 @@
+(defn debug [x] (prn x) x)
+
 (def input (->> (slurp "input.txt")
                 (clojure.string/split-lines)
                 (map #(Integer/parseInt %))))
 
-(defn sum-to [diffs number]
-  (if-let [diff (diffs number)]
-    (reduced [diff number])
-    diffs))
+(defn two-sum [sum numbers]
+  (let [complements (map #(- sum %) numbers)
+        complement? (set complements)]
+    (take 2 (filter complement? numbers))))
 
-
-(def diff-2020 (partial - 2020))
+(defn three-sum [sum [x & xs]]
+  (let [diff (- sum x)
+        pair (two-sum diff xs)]
+    (if-let [[y z] (seq pair)]
+      [x y z]
+      (recur sum xs))))
 
 (defn puzzle1 [numbers]
-  (let [diffs (zipmap (map diff-2020 numbers) numbers)
-        [x y] (reduce sum-to diffs numbers)]
-    (* x y)))
-
-(defn debug [x]
-  (prn x)
-  x)
+  (apply * (two-sum 2020 numbers)))
 
 (defn puzzle2 [numbers]
-  (let [n (count numbers)]
-    (first
-      (for [i (range n) :let [x (nth numbers i)]
-            j (range (inc i) n) :let [y (nth numbers j)]
-            k (range (inc j) n) :let [z (nth numbers k)]
-            :when (= 2020 (+ x y z))]
-        (* x y z)))))
+  (apply * (three-sum 2020 numbers)))
 
 (comment (time (puzzle1 input)))
 (comment (time (puzzle2 [1721 979 366 299 675 1456])))
