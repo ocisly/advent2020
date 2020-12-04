adc53a46a330faad2c0524162ddd5f6c1d95b5a2
Adam Thalhammer <1496836+ocisly@users.noreply.github.com>
2020-12-05 07:36:13 +1100

    fix lost parts of comment

--- 4.clj
+++ 4.clj
@@ -5,7 +5,7 @@
 ;If cm, the number must be at least 150 and at most 193.
 ;If in, the number must be at least 59 and at most 76.
 ;hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
-;ecl (Eye Color) - exactly one of: .
+;ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
 ;pid (Passport ID) - a nine-digit number, including leading zeroes.
 
 (defn debug [x] (prn x) x)
