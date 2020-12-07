9d023675b1d8da941a233039e1b117c8a4f326dd
Adam Thalhammer <1496836+ocisly@users.noreply.github.com>
2020-12-07 19:36:12 +1100

    solve day 7, part 2

--- day-7.clj
+++ day-7.clj
@@ -1,7 +1,8 @@
 (ns day-7
   (:require [hashp.core]
             [clojure.core.logic :as l]
-            [clojure.core.logic.pldb :as pldb]))
+            [clojure.core.logic.pldb :as pldb]
+            [clojure.core.logic.fd :as fd]))
    
 
 (def input (->> (slurp "input7.txt")))
@@ -27,38 +28,41 @@ dotted black bags contain no other bags."))
     {:container head
      :containees (into {} (map parse-spec (remove #{"no other"} specs)))}))
         
-(pldb/db-rel can-contain ^:index p1 ^:index p2)
+(pldb/db-rel can-contain ^:index p1 ^:index p2 n)
 
 
-(defn can-contain-transitive [x y]
+(defn can-contain-transitive [x y n]
   (l/conde
-    [(can-contain x y)]
-    [(l/fresh [z]
-      (can-contain x z)
-      (can-contain-transitive z y))]))
+    [(can-contain x y n)]
+    [(l/fresh [z o p]
+              (can-contain x z o)
+              (can-contain-transitive z y p)
+              (fd/* o p n))]))
 
 (defn query [needle facts]
   (pldb/with-db facts
     (l/run* [q]
-      (can-contain-transitive q needle))))
+      (l/fresh [x y]
+        (can-contain-transitive needle x y)
+        (l/== q [x y])))))
 
 (defn expand-rule [{:keys [container containees]}]
-  (map vector (repeat container) (map key containees)))
+  (map vector (repeat container) containees))
 
 (defn ingest-rule [db [container containee]]
-  (pldb/db-fact db can-contain container containee))
+  (apply pldb/db-fact db can-contain container containee))
 
 (defn ingest [rules]
   (reduce ingest-rule (pldb/db) rules))
 
-(defn puzzle1 [bag rules]
+(defn puzzle2 [bag rules]
   (->> rules
        clojure.string/split-lines
        (map parse-rule)
        (mapcat expand-rule)
        (ingest)
        (query bag)
-       (set)
-       (count)))
+       (map second)
+       (apply +)))
 
-(comment (time (puzzle1 "shiny gold" input)))
+(comment (time (puzzle2 "shiny gold" input)))
