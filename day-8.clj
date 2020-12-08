184e90154de9abfe884b10d78e9b2db25cf82bfe
Adam Thalhammer <1496836+ocisly@users.noreply.github.com>
2020-12-08 23:10:36 +1100

    solve day 8, part 2

--- day-8.clj
+++ day-8.clj
@@ -1,5 +1,4 @@
-(ns day-8
-  (:require [hashp.core]))
+(ns day-8 (:require [hashp.core]))
 
 (def parse-int #(Integer/parseInt %))
 
@@ -21,16 +20,40 @@
     acc))
 
 (defn execute [i acc visited? instructions]
-  (let [instr (instructions i)]
-    (if (visited? i)
-      acc
-      (recur (next-i i instr) (next-acc acc instr) (conj visited? i) instructions))))
+  (cond
+    (visited? i) {:nonterm acc}
+    (>= i (count instructions)) {:term acc}
+    :else (let [instr (instructions i)]
+            (recur (next-i i instr)
+                   (next-acc acc instr)
+                   (conj visited? i)
+                   instructions))))
     
 (defn puzzle1 [program]
   (->> (clojure.string/split-lines program)
        (map parse)
        (vec)
-       (execute 0 0 #{})))
+       (execute 0 0 #{})
+       (:nonterm)))
+
+(defn mangle-nop [instructions]
+  (for [[i {:keys [instr offset]}] (map-indexed vector instructions)
+        :when (= :nop instr)]
+    (assoc instructions i {:instr :jmp :offset offset})))
+
+(defn mangle-jmp [instructions]
+  (for [[i {:keys [instr offset]}] (map-indexed vector instructions)
+        :when (= :jmp instr)]
+    (assoc instructions i {:instr :nop :offset offset})))
+
+(defn puzzle2 [program]
+  (->> (clojure.string/split-lines program)
+       (map parse)
+       (vec)
+       ((juxt mangle-nop mangle-jmp))
+       (apply concat)
+       (map (partial execute 0 0 #{}))
+       (some :term)))
 
 (comment (def input (->> (slurp "input8.txt"))))
 (comment (def input "nop +0
