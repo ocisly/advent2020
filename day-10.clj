27116e2fba70a8ce92232455d03d9b78ac5b1144
Adam Thalhammer <1496836+ocisly@users.noreply.github.com>
2020-12-11 13:28:35 +1100

    Refactor, solve part 2

--- day-10.clj
+++ day-10.clj
@@ -5,19 +5,43 @@
 (defn select-vals [keyseq m]
   (for [k keyseq] (m k)))
 
-(defn puzzle1 [input]
-  (->> (clojure.string/split-lines input)
-       (map parse-int)
-       (sort)
-       (cons 0)
+(defn diffs [joltages]
+  (->> joltages
        (partition 2 1)
-       (map reverse)
-       (map (partial apply -))
-       (cons 3)
-       (frequencies)
-       (select-vals [1 3])
-       (apply *)))
+       (map (comp (partial apply -) reverse))))
+
+(defn puzzle1 [input]
+  (let [joltages (->> (clojure.string/split-lines input) (map parse-int) sort (cons 0) vec)
+        joltages (conj joltages (+ (last joltages) 3))]
+    (->> (diffs joltages)
+         (frequencies)
+         (select-vals [1 3])
+         (apply *))))
+
+; from https://programming-idioms.org/idiom/67/binomial-coefficient-n-choose-k/2781/clojure#
+(defn binom [n k]
+  (let [fact #(apply * (range 1 (inc %)))]
+    (/ (fact n)
+       (* (fact k) (fact (- n k))))))
+
+(defn combos [min-k n]
+  (let [all (int (.pow 2M n))
+        too-short (apply + (for [k (range min-k (inc n))] (binom n k)))]
+    (- all too-short)))
+
+(defn puzzle2 [input]
+  (let [joltages (->> (clojure.string/split-lines input) (map parse-int) sort (cons 0) vec)
+        joltages (conj joltages (+ (last joltages) 3))]
+    (->> (diffs joltages)
+         (partition-by identity)
+         (filter (comp #{1} first))
+         (map count)
+         (filter #(>= % 2))
+         (map dec)
+         (map (partial combos 3))
+         (apply *))))
 
 (def input (slurp "input10.txt"))
 
-(puzzle1 input)
+(comment (puzzle1 input))
+(comment (puzzle2 input))
