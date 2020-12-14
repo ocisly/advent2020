cfbfbb73a0a062367115bf349e0a254e967b2c45
Adam Thalhammer <1496836+ocisly@users.noreply.github.com>
2020-12-15 08:42:09 +1100

    Solve day 13, part 2

--- day-13.clj
+++ day-13.clj
@@ -1,9 +1,6 @@
 (ns day-13 (:require [hashp.core]))
 
 (def parse-int #(Long/parseLong %))
-(def input (slurp "input13.txt"))
-(comment (def input "939
-7,13,x,x,59,x,31,19"))
 
 (defn departure-times [bus]
   {:id bus :times (reductions + 0 (repeat bus))})
@@ -21,4 +18,56 @@
          (apply min-key :earliest)
          (#(* (:id %) (- (:earliest %) dep))))))
 
+(defn parse-bus [index string]
+  (when (not= string "x")
+    {:index index :id (parse-int string)}))
+
+; For some reason, Math/abs does not work properly
+(defn abs [n]
+  (if (neg? n) (- n) n))
+
+; from https://rosettacode.org/wiki/Modular_inverse#Clojure
+(defn extended-gcd [a b]
+  (cond (zero? a) [(abs b) 0 1]
+        (zero? b) [(abs a) 1 0]
+        :else (loop [s 0
+                     s0 1
+                     t 1
+                     t0 0
+                     r (abs b)
+                     r0 (abs a)]
+                (if (zero? r)
+                  [r0 s0 t0]
+                  (let [q (quot r0 r)]
+                    (recur (- s0 (* q s)) s
+                           (- t0 (* q t)) t
+                           (- r0 (* q r)) r))))))
+
+; from https://rosettacode.org/wiki/Modular_inverse#Clojure
+(defn mul-inv [a b]
+  (let [b (if (neg? b) (- b) b)
+        a (if (neg? a) (- b (mod (- a) b)) a)
+        egcd (extended-gcd a b)]
+      (when (= (first egcd) 1)
+        (mod (second egcd) b))))
+
+; adapted from https://rosettacode.org/wiki/Chinese_remainder_theorem#Python
+(defn chinese-remainder [n a]
+  (let [prod (apply * n)
+        terms (for [[x y] (map vector n a)
+                    :let [p (quot prod x)]]
+                  (* y (mul-inv p  x) p))]
+    (mod (apply + terms) prod)))
+
+(defn puzzle2 [in]
+  (let [[dep buses](clojure.string/split-lines in)
+        dep (parse-int dep)
+        buses (map-indexed parse-bus (clojure.string/split buses #","))]
+    (->> (filter some? buses)
+         (map (juxt :id #(mod (- (:id %) (:index %)) (:id %))))
+         (apply map vector)
+         (apply chinese-remainder))))
+        
+(def input (slurp "input13.txt"))
 (puzzle1 input)
+(puzzle2 input)
