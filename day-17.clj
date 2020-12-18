f79b4127b48a67a93de6a6286ca9dcb4680015ee
Adam Thalhammer <1496836+ocisly@users.noreply.github.com>
2020-12-18 20:19:03 +1100

    Solve day 17, part 2

--- day-17.clj
+++ day-17.clj
@@ -14,34 +14,20 @@
                :when (active? ((board y) x))]
           {:x (- mid-x x)
            :y (- mid-y y)
-           :z 0}))))
-
-(defn sparse->dense [sparse]
-  (let [[min-x max-x] (apply (juxt min max) (map :x sparse))
-        [min-y max-y] (apply (juxt min max) (map :y sparse))
-        [min-z max-z] (apply (juxt min max) (map :z sparse))]
-    (for [z (range min-z (inc max-z))]
-      (into [] (for [y (reverse (range min-y (inc max-y)))]
-                 (into [] (for [x (reverse (range min-x (inc max-x)))]
-                            (if (sparse {:x x :y y :z z})
-                              \#
-                              \.))))))))
-
-(comment (defn reshuffle [transform-fn board]
-          (let [indices (for [x (range (count board))
-                              y (range (count (board 0)))]
-                          [x y])]
-            (reduce evolve board indices))))
+           :z 0
+           :w 0}))))
 
 (defn neighbors [place sparse]
   (for [dx [-1 0 1]
         dy [-1 0 1]
         dz [-1 0 1]
+        dw [-1 0 1]
         :let [cube {:x (+ (:x place) dx)
                     :y (+ (:y place) dy)
-                    :z (+ (:z place) dz)}
+                    :z (+ (:z place) dz)
+                    :w (+ (:w place) dw)}
               s (sparse cube)]
-        :when (and (not= 0 dx dy dz) s)]
+        :when (and (not= 0 dx dy dz dw) s)]
     s))
 
 (defn transform [place new-sparse sparse]
@@ -57,26 +43,24 @@
   (let [[min-x max-x] (apply (juxt min max) (map :x sparse))
         [min-y max-y] (apply (juxt min max) (map :y sparse))
         [min-z max-z] (apply (juxt min max) (map :z sparse))
+        [min-w max-w] (apply (juxt min max) (map :w sparse))
         indices (for [x (range (dec min-x) (inc (inc max-x)))
                       y (range (dec min-y) (inc (inc max-y)))
-                      z (range (dec min-z) (inc (inc max-z)))]
-                    {:x x :y y :z z})
+                      z (range (dec min-z) (inc (inc max-z)))
+                      w (range (dec min-w) (inc (inc max-w)))]
+                    {:x x :y y :z z :w w})
         evolve' (fn [new-sparse place]
                   (transform place new-sparse sparse))]
     (reduce evolve' sparse indices)))
 
-(defn puzzle1 [in]
+(defn puzzle2 [in]
   (->> (clojure.string/split-lines in)
        (mapv vec)
        (dense->sparse)
        (iterate evolve)
        (#(nth % 6))
        (count)))
-       ;(sparse->dense)
-       ;(map (partial map clojure.string/join))
-       ;(map #(clojure.string/join "\n" %))
-       ;(map #(do (println) (println %)))))
 
 (def input (slurp "input17.txt"))
-(time (puzzle1 input))
+(time (puzzle2 input))
 (comment (time (puzzle2 input)))
