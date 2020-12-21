e95af2d3f1d6b15255786934520a299adc43e291
Adam Thalhammer <1496836+ocisly@users.noreply.github.com>
2020-12-21 19:10:19 +1100

    solve day 21, part 2

--- day-21.clj
+++ day-21.clj
@@ -35,5 +35,34 @@
        (map parse-ingredients)
        (solve1)))
 
+(defn find-ingredient [acc remaining [x & xs]]
+  (let [seen (set (keys acc))
+        allergen (:allergen x)
+        [i & is] (clojure.set/difference (:ingredients x) seen)]
+    (cond
+      (and (nil? x) (empty? remaining)) acc
+      (nil? x) (recur acc '() remaining)
+      (empty? is) (recur (assoc acc i allergen) remaining xs)
+      :else (recur acc (conj remaining x) xs))))
+
+(defn solve2 [ingredients]
+  (->> ingredients
+       (map allergen->ingredients)
+       (intersect)
+       (map #(zipmap [:allergen :ingredients] %))
+       (sort-by (comp count :ingredients))
+       (find-ingredient {} '())
+       (map #(zipmap [:ingredient :allergen] %))
+       (sort-by :allergen)
+       (map :ingredient)
+       (clojure.string/join ",")))
+
+(defn puzzle2 [input]
+  (->> input
+       clojure.string/split-lines
+       (map parse-ingredients)
+       (solve2)))
+
 (def input (->> (slurp "input21.txt")))
 (time (puzzle1 input))
+(time (puzzle2 input))
